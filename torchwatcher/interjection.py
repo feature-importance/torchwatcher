470e5eebc8b3c775359ee3c5af2fd04c261b0d72
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-02-12 21:02:48 +0000

    Sketching out different interjection models for adding to the graph

--- torchwatcher/interjection.py
+++ torchwatcher/interjection.py
@@ -1,40 +1,74 @@
 import abc
 
 import torch
+import torch.nn as nn
 
 
 def x_if_ix_is_none(x, xp):
     return x if xp is None else xp
 
-class AbstractInterjection(abc.ABC):
+
+class AbstractInterjection(nn.Module, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def process(self, x, *args, **kwargs):
+    def process(self, *args, **kwargs):
         pass
 
-class BasicForwardInterjection(AbstractInterjection):
+
+class ForwardInterjection(AbstractInterjection):
     def __init__(self):
         super().__init__()
 
     @abc.abstractmethod
-    def process(self, x, y=None, *args, **kwargs):
+    def process(self, *args, **kwargs):
         pass
 
-    def forward(self, x):
-        return x_if_ix_is_none(x, self.process(x))
+    def forward(self, *args, **kwargs):
+        return x_if_ix_is_none(args, self.process(*args, **kwargs))
 
 
 class WrappedForwardInterjection(AbstractInterjection):
     def __init__(self):
         super().__init__()
-        self.wrapped: [torch.fx.GraphModule | None] = None
+        self.wrapped = None
+
+    @property
+    def wrapped(self) -> [torch.fx.GraphModule | None]:
+        return self._wrapped
+
+    @wrapped.setter
+    def wrapped(self, value: torch.fx.GraphModule):
+        self._wrapped = value
 
     @abc.abstractmethod
-    def process(self, x, y=None, *args, **kwargs):
+    def process(self, *args, **kwargs):
         pass
 
-    def forward(self, x):
-        y = self.wrapped(x)
+    def forward(self, *args, **kwargs):
+        y = self.wrapped(*args, **kwargs)
 
-        return x_if_ix_is_none(y, self.process(x, y))
+        return x_if_ix_is_none(y, self.process(*args, **kwargs))
 
+
+class WrappedForwardBackwardInterjection(WrappedForwardInterjection):
+    def __init__(self):
+        super().__init__()
+        self.wrapped: [torch.fx.GraphModule | None] = None
+        self.hook = None
+
+    @abc.abstractmethod
+    def process_backward(self,
+                         grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
+                         grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] |
+                                                                                     torch.Tensor | None]:
+        pass
+
+    @WrappedForwardInterjection.wrapped.setter
+    def wrapped(self, value: torch.fx.GraphModule):
+        super().wrapped(value)
+
+        def hook(module: nn.Module,
+                 grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
+                 grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] | torch.Tensor | None]:
+            self.process_backward(grad_input, grad_output)
+        self.hook = value.register_full_backward_hook(hook)
 
