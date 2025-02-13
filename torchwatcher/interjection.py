b1d82be93e4edc5b1e8821b66a47e081263cf8c5
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-02-13 17:51:44 +0000

    working on fleshing out tracing

--- torchwatcher/interjection.py
+++ torchwatcher/interjection.py
@@ -14,7 +14,7 @@ def unpack(result):
 
 class AbstractInterjection(nn.Module, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def process(self, *args, **kwargs):
+    def process(self, name, *args, **kwargs):
         pass
 
 
@@ -23,56 +23,51 @@ class ForwardInterjection(AbstractInterjection):
         super().__init__()
 
     @abc.abstractmethod
-    def process(self, *args, **kwargs):
+    def process(self, name, *args, **kwargs):
         pass
 
-    def forward(self, *args, **kwargs):
-        return unpack(x_if_xp_is_none(args, self.process(*args, **kwargs)))
+    def forward(self, name, *args, **kwargs):
+        return unpack(x_if_xp_is_none(args, self.process(name, *args, **kwargs)))
 
 
 class WrappedForwardInterjection(AbstractInterjection):
     def __init__(self):
         super().__init__()
-        self.wrapped = None
+        self._wrapped: dict[str, torch.fx.GraphModule] = {}
 
-    @property
-    def wrapped(self) -> [torch.fx.GraphModule | None]:
-        return self._wrapped
-
-    @wrapped.setter
-    def wrapped(self, value: torch.fx.GraphModule):
-        self._wrapped = value
+    def wrap(self, name: str, module: torch.fx.GraphModule):
+        self._wrapped[name] = module
 
     @abc.abstractmethod
-    def process(self, *args, **kwargs):
+    def process(self, name, *args, **kwargs):
         pass
 
-    def forward(self, *args, **kwargs):
-        y = self.wrapped(*args, **kwargs)
+    def forward(self, name, *args, **kwargs):
+        y = self.wrapped[name](*args, **kwargs)
 
-        return unpack(x_if_xp_is_none(y, self.process(*args, **kwargs)))
+        return unpack(x_if_xp_is_none(y, self.process(name, *args, **kwargs)))
 
 
-class WrappedForwardBackwardInterjection(WrappedForwardInterjection):
-    def __init__(self):
-        super().__init__()
-        self.wrapped: [torch.fx.GraphModule | None] = None
-        self.hook = None
-
-    @abc.abstractmethod
-    def process_backward(self,
-                         grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
-                         grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] |
-                                                                                     torch.Tensor | None]:
-        pass
-
-    @WrappedForwardInterjection.wrapped.setter
-    def wrapped(self, value: torch.fx.GraphModule):
-        super().wrapped(value)
-
-        def hook(module: nn.Module,
-                 grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
-                 grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] | torch.Tensor | None]:
-            self.process_backward(grad_input, grad_output)
-        self.hook = value.register_full_backward_hook(hook)
-
+# class WrappedForwardBackwardInterjection(WrappedForwardInterjection):
+#     def __init__(self):
+#         super().__init__()
+#         self.wrapped: [torch.fx.GraphModule | None] = None
+#         self.hook = None
+#
+#     @abc.abstractmethod
+#     def process_backward(self,
+#                          grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
+#                          grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] |
+#                                                                                      torch.Tensor | None]:
+#         pass
+#
+#     @WrappedForwardInterjection.wrapped.setter
+#     def wrapped(self, value: torch.fx.GraphModule):
+#         super().wrapped(value)
+#
+#         def hook(module: nn.Module,
+#                  grad_input: [tuple[torch.Tensor, ...] | torch.Tensor],
+#                  grad_output: [tuple[torch.Tensor, ...] | torch.Tensor]) -> [tuple[torch.Tensor] | torch.Tensor | None]:
+#             self.process_backward(grad_input, grad_output)
+#         self.hook = value.register_full_backward_hook(hook)
+#
--- torchwatcher/tracing.py
+++ torchwatcher/tracing.py
@@ -1,32 +1,71 @@
+import copy
+
 import torch
 from torch import nn
 from torch.fx import symbolic_trace
 from torchvision.models import resnet18
 
-from torchwatcher.interjection import ForwardInterjection
+from torchwatcher.interjection import ForwardInterjection, WrappedForwardInterjection
 
 
-def get_fresh_qualname(graph: torch.fx.Graph, prefix: str) -> str:
+def get_fresh_qualname(traced: torch.fx.GraphModule, prefix: str) -> str:
+    """
+    Generate a unique qualified name for a module that will be added to the graph.
+
+    Args:
+        traced ():
+        prefix ():
+
+    Returns:
+
+    """
     i = 0
     while True:
         qualname = f"{prefix}{i}"
         i += 1
-        if not hasattr(graph, qualname):
+        if not hasattr(traced, qualname):
             break
 
     return qualname
 
+def find_nodes(graph: torch.fx.Graph, op):
+    return list(filter(lambda n: n.op == op ,graph.nodes))
 
-def insert_interjection(traced, node, interjection):
-    if hasattr(interjection, 'wrapped'):
-        raise RuntimeError("Not yet implemented")
+
+def extract_node(traced: torch.fx.GraphModule, target_node: torch.fx.Node) -> torch.fx.GraphModule:
+    gm = copy.deepcopy(traced)
+
+    # Set new inputs with placeholders
+    for node in gm.graph.nodes:
+        if node.name == target_node.prev.name:
+            node.op, node.target, node.args, node.kwargs = 'placeholder', node.name, (), {}
+
+    output_node = find_nodes(gm.graph, op='output')[0]
+    output_node.args = tuple([node for node in gm.graph.nodes if node.name == target_node.name])
+    gm.graph.eliminate_dead_code()
+
+    # Remove unused placeholders
+    for node in find_nodes(gm.graph, op='placeholder'):
+        if node.name != target_node.prev.name:
+            gm.graph.erase_node(node)
+    new_module = torch.fx.GraphModule(gm, gm.graph)
+
+    return new_module
+
+def insert_interjection(traced, node, interjection, interjection_name):
+    if hasattr(interjection, '_wrapped'):
+        with traced.graph.inserting_after(node):
+            # new node to represent the call to the interjection
+            new_node = traced.graph.call_module(interjection_name, (node.name, node,))
+            # register the extracted node that we wrap
+            interjection.wrap(node.name, extract_node(traced, node))
+            # clean everything up by replacing uses and inputs, then removing the original node
+            node.replace_all_uses_with(new_node)
+            new_node.replace_input_with(new_node, node.prev)
+        traced.graph.erase_node(node)
     else:
         with traced.graph.inserting_after(node):
-            name = get_fresh_qualname(traced.graph, type(interjection).__name__)
-            # TODO: how do we reuse the interjections so we just have one module instance and feed in the position in
-            #  the graph?
-            traced.add_module(name, interjection)
-            new_node = traced.graph.call_module(name, (node,))
+            new_node = traced.graph.call_module(interjection_name, (node.name, node,))
             node.replace_all_uses_with(new_node)
             new_node.replace_input_with(new_node, node)
 
@@ -35,24 +74,39 @@ def interject_by_module_class(network, target_module_class, interjection):
     traced = symbolic_trace(network)
     modules = dict(traced.named_modules())
 
+    # Any given interjection is added as a module just once; when it is called, the name of the node is passed along so
+    # that if it's reused across multiple points the user can tell where its coming from.
+    interjection_name = get_fresh_qualname(traced, type(interjection).__name__)
+    traced.add_module(interjection_name, interjection)
+
     for node in traced.graph.nodes:
         if node.target not in modules:
             continue
 
         if type(modules[node.target]) == target_module_class:
-            insert_interjection(traced, node, interjection)
+            insert_interjection(traced, node, interjection, interjection_name)
 
     traced.recompile()
     return traced
 
 
 class MyForwardInterjection(ForwardInterjection):
-    def process(self, *args, **kwargs):
-        print(args[0])
+    def process(self, name, *args, **kwargs):
+        print(name)
         # return args[0]
+
+class MyWrappedForwardInterjection(WrappedForwardInterjection):
+    def process(self, name, *args, **kwargs):
+        print(name)
 
 
 net = resnet18()
 net2 = interject_by_module_class(net, nn.Conv2d, MyForwardInterjection())
-
+# print(net2.code)
 r = net2(torch.zeros(1, 3, 224, 244))
+
+net = resnet18()
+net2 = interject_by_module_class(net, nn.Conv2d, MyWrappedForwardInterjection())
+# print(net2.code)
+net2(torch.zeros(1, 3, 224, 244))
+
