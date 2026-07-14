fix drawing and gradient bugs for analysers

examples/Gradient_accumulation.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.23.14"
+__generated_with = "0.23.11"
 app = marimo.App(width="medium")
 
 
@@ -8,10 +8,20 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
 
+    import torch
+    import torch.nn as nn
+
+    from pathlib import Path
+
+    from model_utilities.models.cifar_resnet import resnet18_3x3, ResNet18_3x3_Weights 
+    from model_utilities.datasets import cifar10_loaders
+
     from torchwatcher.analysis.analysis import Analyzer, AnalyzerState, PerClassAnalyzer
     from torchwatcher.analysis.running_stats import Variance
     from torchwatcher.interjection import interject_by_match
     from torchwatcher.interjection.node_selector import is_activation
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     class GradientAccumulationAnalyzer(Analyzer):
         def __init__(self):
@@ -35,12 +45,18 @@ def _():
             rec['mean'] = result.mean()
             return rec
 
-
     return (
         GradientAccumulationAnalyzer,
+        Path,
         PerClassAnalyzer,
+        ResNet18_3x3_Weights,
+        cifar10_loaders,
+        device,
         interject_by_match,
         is_activation,
+        mo,
+        resnet18_3x3,
+        torch,
     )
 
 
@@ -48,57 +64,51 @@ def _():
 def _(
     GradientAccumulationAnalyzer,
     PerClassAnalyzer,
+    ResNet18_3x3_Weights,
+    device,
     interject_by_match,
     is_activation,
+    resnet18_3x3,
 ):
-    from model_utilities.models.cifar_resnet import resnet18_3x3, ResNet18_3x3_Weights 
     grad_analyzer = GradientAccumulationAnalyzer()
     analyzer = PerClassAnalyzer(grad_analyzer)
     model = resnet18_3x3(weights=ResNet18_3x3_Weights.CIFAR10_s0)
-    imodel = interject_by_match(model, is_activation, analyzer).to("cuda:0")
-
+    imodel = interject_by_match(model, is_activation, analyzer).to(device)
     return analyzer, imodel
 
 
 @app.cell
-def _(analyzer, imodel):
-    from model_utilities.datasets import cifar10_loaders
-    import torch
-
-    train_loader, val_loader = cifar10_loaders('/home/am1g15/data/cifar10', batch_size=4)
+def _(Path, analyzer, cifar10_loaders, device, imodel, torch):
+    train_loader, val_loader = cifar10_loaders(Path('~/data/').expanduser(), batch_size=4)
     loss = torch.nn.CrossEntropyLoss()
 
     for x, y in val_loader:
-        y = y.to("cuda:0")
+        y = y.to(device)
         analyzer.targets = y
-        pred = imodel(x.to("cuda:0"))
-        loss(pred, y).backward()    
-
-        print(analyzer.to_dict())
+        pred = imodel(x.to(device))
+        loss(pred, y).backward() 
         break
 
-
-    
+    print(analyzer.to_dict())
     return
 
 
 @app.cell
 def _(analyzer):
-    analyzer.analyzers[list(analyzer.analyzers.keys())[0]].working_results
+    analyzer.to_dict()[0]['layer4.1.relu_1']["var"].shape
     return
 
 
 @app.cell
-def _(analyzer):
-    analyzer.to_dict()['layer4.1.relu_1']["var"].shape
+def _(imodel, mo, torch):
+    from torchwatcher.drawing import draw_graph_pretty
+
+    mo.Html(draw_graph_pretty(imodel, torch.empty(1,3,32,32)).create_svg().decode())
     return
 
 
 @app.cell
-def _(imodel):
-    from torchwatcher.drawing import draw_graph
-
-    draw_graph(imodel)
+def _():
     return
 
 

src/torchwatcher/drawing.py
@@ -8,6 +8,7 @@ import torch
 from torch.fx.passes.graph_drawer import FxGraphDrawer, _WEIGHT_TEMPLATE, _COLOR_MAP, _HASH_COLOR_MAP
 from torch.fx.passes.shape_prop import TensorMetadata
 from torchwatcher.analysis import Analyzer
+from torchwatcher.analysis.analysis import PerClassAnalyzer
 from torchwatcher.interjection.tracing import DualGraphModule, trace_shapes, ShapeProp
 from torchwatcher.interjection import WrappedForwardInterjection, ForwardInterjection
 
@@ -33,7 +34,7 @@ def compact_list_repr(x: list[Any]) -> str:
 
 class SubmoduleFxGraphDrawer(FxGraphDrawer):
     """
-    FxGraphDrawer extended to show the submodules of the original code as traced by the torchwatcher.
+    FxGraphDrawer extended to show the submodules of the original code as traced by torchwatcher.
     """
 
     def _get_node_style(self, node: torch.fx.Node) -> dict[str, str]:
@@ -117,10 +118,10 @@ class SubmoduleFxGraphDrawer(FxGraphDrawer):
             if isWrapped:
                 dot_node = pydot.Cluster(
                     name + "/" + node.name,
-                    # label=node.name,
-                    label=self._get_node_label(
-                        graph_module, node, skip_node_names_in_args, parse_stack_trace
-                    ),
+                    label=node.name + "\n" + str(type(md))[8:-2],
+                    # label=self._get_node_label(
+                    #     graph_module, node, skip_node_names_in_args, parse_stack_trace
+                    # ),
                     fillcolor='MistyRose1',
                     penwidth= "1",
                     style= "solid,filled",
@@ -135,7 +136,8 @@ class SubmoduleFxGraphDrawer(FxGraphDrawer):
 
                 wrapped_nodes.add(node)
 
-                ShapeProp(gm).propagate(torch.empty(node.meta['tensor_meta'].shape))
+                if 'tensor_meta' in node.meta:
+                    ShapeProp(gm).propagate(torch.empty(node.meta['tensor_meta'].shape))
                 for n in gm.graph.nodes:
                     if n.op != 'placeholder' and n.op != 'output':
                         dot_node.add_node(pydot.Node(name + "/" + node.name+"/"+n.name,
@@ -400,7 +402,7 @@ def draw_graph(
         skip_node_names_in_args: bool = True,
         parse_stack_trace: bool = False,
         dot_graph_shape: Optional[str] = None,
-        normalize_args: bool = False):
+        normalize_args: bool = False) -> pydot.Dot:
 
     return SubmoduleFxGraphDrawer(module.graphmodule, module.__class__.__name__, ignore_getattr,
                                   ignore_parameters_and_buffers, skip_node_names_in_args,
@@ -413,7 +415,7 @@ def draw_graph_pretty(
         show_shapes: bool = True,
         show_node_names: bool = False,
         show_extra_info: bool = False,
-        ):
+        ) -> pydot.Dot:
     if show_shapes:
         trace_shapes(module, *inputs)
 